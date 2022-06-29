e6ab661f3f76e8a392b5fe4d360d28b126725c42
Bazyli Kot <ciobab97@gmail.com>
2022-06-29 22:27:01 +0200

    update print var with os info

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return f"Hello World! Env: {os.environ}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
